35136b7299b6150bc18db9a7e03f4de8abfc4ad3
jiaowoshabi <wenhaoz@g.ucla.edu>
2019-04-28 22:46:34 -0700

    use mod to convert negative count between 0~length-1

--- q19.ml
+++ q19.ml
@@ -1,11 +1,10 @@
 let rotate lst count =
   let rec helper lst count acc =
-    match (compare count 0), lst with
+    match count, lst with
     | _, [] -> List.rev acc
     | 0, hd::tl -> (hd::tl) @ (List.rev acc)
-    | -1, hd::tl -> helper (hd::tl) ((List.length (hd::tl))+count) []
-    | 1, hd::tl -> helper tl (count-1) (hd::acc)
-  in helper lst count []
+    | a, hd::tl -> helper tl (a-1) (hd::acc)
+  in helper lst (((count mod (List.length lst)) + (List.length lst)) mod (List.length lst)) []
    
 let test0 = rotate ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"] 3 = ["d"; "e"; "f"; "g"; "h"; "a"; "b"; "c"];;
 let test1 = rotate ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"] (-2) = ["g"; "h"; "a"; "b"; "c"; "d"; "e"; "f"];;
